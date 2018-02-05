356cb3d17ff89d60a8743bb22cc3513afdd61505
Daniil <gatorr15@ya.ru>
2018-02-05 13:29:00 +0300

    refactoring

--- bot.php
+++ bot.php
@@ -1,45 +1,48 @@
 <?php
-header('Content-Type: multipart/form-data');
+
 $token = '503700120:AAGPUE5Qb-IIt8qQyM92I9h_llLk-UPQf0c';
 $api = 'https://api.telegram.org/bot'.$token;
 
-//$output = json_decode(file_get_contents('php://input'), TRUE); //сюда приходят все запросы по вебхукам
-$jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(parse_str($_POST)), RecursiveIteratorIterator::SELF_FIRST);
+$input = file_get_contents('php://input');
+$output = json_decode($input, TRUE); //сюда приходят все запросы по вебхукам
+
+$data = array();
 
 //телеграмные события
-// $chat_id = $output['message']['chat']['id']; //отделяем id чата, откуда идет обращение к боту
-// $message_id = $output['message']['message_id']; //id сообщения, которое нужно редактировать
-// $message = $output['message']['text']; //сам текст сообщения
-// $user = $output['message']['from']['username']; //сюда кладем юзернейм человек, обратившегося к боту
-// $user_firstname = $output['message']['from']['first_name'];
-// $user_id = $output['message']['from']['id']; //id юзера, для банов
+$chat_id = $output['message']['chat']['id']; //отделяем id чата, откуда идет обращение к боту, я = 197416875
+$message_id = $output['message']['message_id']; //id сообщения, которое нужно редактировать
+$message = $output['message']['text']; //сам текст сообщения
+$user = $output['message']['from']['username']; //сюда кладем юзернейм человек, обратившегося к боту
+$user_firstname = $output['message']['from']['first_name'];
+$user_id = $output['message']['from']['id']; //id юзера, для банов
 
-// //события ACR
+//события ACR
 
-// $message = mb_strtolower($message); //этим унифицируем любое входящее сообщение в нижний регистр для дальнейшей обработки без ебли с кейсами
+$message = mb_strtolower($message); //этим унифицируем любое входящее сообщение в нижний регистр для дальнейшей обработки без ебли с кейсами
 
 //--ДАЛЬШЕ ЛОГИКА БОТА--//
 
-if ($message == "/start") {
-	sendMessage('197416875', "YO BRAH");
-}
+preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
+$blocks = preg_split("/-+$boundary", $input);
+array_pop($blocks);
 
-if ($message == "/chat") {
-	sendMessage($chat_id, $chat_id);
-}
-
-if (!empty($jsonIterator)) {
-	sendMessage('197416875', $jsonIterator);
-	foreach ($jsonIterator as $key => $value) {
-		if (is_array($value)) {
-			echo 'arr.'.$key;
-			sendMessage('197416875', 'arr.'.$key);
-		} else {
-			echo $key.': '.$value;
-			sendMessage('197416875', $key.': '.$value);
-		}
+foreach ($blocks as $id => $block) {
+	if (empty($block)) {
+		continue;
 	}
+
+	if (strpos($block, 'application/octet-stream') !== FALSE) {
+		preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+	} else {
+		preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+	}
+	$data[$matches[1]] = $matches[2];
 }
+
+foreach ($data as $key => $value) {
+	sendMessage('197416875', $key.': '.$value);
+}
+
 
 //----------------------------------------------------------------------------------------------------------------------------------//
 
@@ -58,8 +61,7 @@ function deleteMessage($chat_id, $message_id)
 //отправка простого сообщения
 function sendMessage($chat_id, $message)
 {
+	header('Content-Type: text/html; charset: utf-8;');
 	file_get_contents($GLOBALS['api'].'/sendMessage?chat_id='.$chat_id.'&text='.urlencode($message));
 }
-
-echo '<script>console.log(\'End script\')</script>';
 ?>
