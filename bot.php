8a3e9f446b83db0ae4b37ae2ecc8789b32bde9d2
Daniil <gatorr15@ya.ru>
2018-09-07 16:28:46 +0300

    Hopeful fix

--- bot.php
+++ bot.php
@@ -34,7 +34,7 @@ $ACR_fields = array(
 
 if ($ACR_fields['source'] == 'ACR') {
 	$voice_file = $_FILES['file'];
-	sendMessage(197416875, $ACR_fields['acrfilename'].', '.$ACR_fields['date'].', '.$ACR_fields['contact'].', '.$ACR_fields['phone'].', '.$ACR_fields['direction'].', '.$ACR_fields['duration']);
+	sendMessage(197416875, realpath($_FILES['file']).', '.$ACR_fields['acrfilename'].', '.$ACR_fields['date'].', '.$ACR_fields['contact'].', '.$ACR_fields['phone'].', '.$ACR_fields['direction'].', '.$ACR_fields['duration']);
 	sendVoice(197416875, $voice_file, $ACR_Fields['contact']);
 }
 
@@ -60,7 +60,7 @@ function sendMessage($chat_id, $message)
 
 //отправка разговора
 function sendVoice($chat_id, $voice, $caption) {
-	$filepath = realpath($_FILES['file']['name']);
+	$filepath = realpath($_FILES['file']);
 	$post_data = array(
 		'chat_id' => $chat_id,
 		'voice' => new CURLFile($filepath),
