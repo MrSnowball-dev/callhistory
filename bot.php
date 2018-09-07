4b3d1ca7079102730d0de1fe74a398e2ef937644
Daniil <gatorr15@ya.ru>
2018-09-07 15:06:11 +0300

    Debug message for upload

--- bot.php
+++ bot.php
@@ -41,7 +41,7 @@ $ACR_fields = array(
 
 if ($ACR_fields['source'] == 'ACR') {
 	$voice_file = $_FILES['file'];
-	sendMessage(197416875, 'TEST, '.$_FILES['file']['name']);
+	sendMessage(197416875, $ACR_fields['acrfilename'].', '.$ACR_fields['date'].', '.$ACR_fields['contact'].', '.$ACR_fields['phone'].', '.$ACR_fields['direction'].', '.$ACR_fields['duration'].', ');
 	sendVoice(197416875, $voice_file, $_POST['phone']);
 }
 
