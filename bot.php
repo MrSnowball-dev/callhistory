e2cf64166c6d01a0c8b074fa6ac998af269cffc7
Daniil <gatorr15@ya.ru>
2018-09-07 11:57:11 +0300

    Reviving development
    Trying tings I failed at last time. Hopefully with more understanding of how things work :|

--- bot.php
+++ bot.php
@@ -1,6 +1,5 @@
 <?php
-header('Content-Type: multipart/form-data; boundary="--boundary"');
-$token = '503700120:AAGPUE5Qb-IIt8qQyM92I9h_llLk-UPQf0c';
+$token = '503700120:AAGxJuN9CMFqNjQ2lOsLvtb79T-Llz3H130';
 $api = 'https://api.telegram.org/bot'.$token;
 
 $input = file_get_contents('php://input');
@@ -10,9 +9,6 @@ $output = json_decode($input, TRUE); //сюда приходят все запр
 $chat_id = $output['message']['chat']['id']; //отделяем id чата, откуда идет обращение к боту, я = 197416875
 $message_id = $output['message']['message_id']; //id сообщения, которое нужно редактировать
 $message = $output['message']['text']; //сам текст сообщения
-$user = $output['message']['from']['username']; //сюда кладем юзернейм человек, обратившегося к боту
-$user_firstname = $output['message']['from']['first_name'];
-$user_id = $output['message']['from']['id']; //id юзера, для банов
 
 //события ACR
 
@@ -20,31 +16,11 @@ $message = mb_strtolower($message); //этим унифицируем любое
 
 //--ДАЛЬШЕ ЛОГИКА БОТА--//
 
-$data = array();
-preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
-if (!count($matches)) {
-	parse_str(urldecode($input), $data);
-}
+$input_contents = [];
+mb_parse_str($input, $input_contents);
 
-$boundary = $matches[1];
-$blocks = preg_split("/-+$boundary/", $input);
-array_pop($blocks);
-
-foreach ($blocks as $id => $block) {
-	if (empty($block)) {
-		continue;
-	}
-	if (strpos($block, 'application/octet-stream') !== FALSE) {
-		preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
-		$data['files'][$matches[1]] = $matches[2];
-	} else {
-		preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
-		$data[$matches[1]] = $matches[2];
-	}
-}
-
-foreach ($data as $key => $value) {
-	sendMessage('197416875', 'input '.$key.': '.$value);
+foreach($input_contents as $parsed) {
+	sendMessage(197416875, $parsed);
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------//
@@ -66,5 +42,9 @@ function sendMessage($chat_id, $message)
 {
 	header('Content-Type: text/html; charset=utf-8');
 	file_get_contents($GLOBALS['api'].'/sendMessage?chat_id='.$chat_id.'&text='.urlencode($message));
+}
+
+function sendVoice() {
+	
 }
 ?>
