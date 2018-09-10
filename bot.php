23888465b8f5df798c6264b6e003f581a01a63e2
Daniil <gatorr15@ya.ru>
2018-09-10 13:18:50 +0300

    Fixing creating new user

--- bot.php
+++ bot.php
@@ -31,10 +31,10 @@ if ($message == '/start') {
 	while ($sql = mysqli_fetch_object($query)) {
 		$sql_chat_id = $sql->chat_id;
 	}
-	if ($sql_chat_id != '') {
+	if ($sql_chat_id == $chat_id) {
 		sendMessage($chat_id, "Вы уже были зарегистрированы!\n\nВведите /secret чтобы узнать secret для настройки ACR.");
 	} else {
-		mysqli_query($db, 'insert into users (chat_id, acr_secret) values ('.$chat_id.', '.$acr_secret.')');
+		mysqli_query($db, "insert into users (chat_id, acr_secret) values (".$chat_id.", '".$acr_secret."')");
 		sendMessage($chat_id, "Вы зарегистрированы!\n\nВведите /secret чтобы узнать secret для настройки ACR.\n\n".$chat_id."\n".$acr_secret);
 	}
 	
