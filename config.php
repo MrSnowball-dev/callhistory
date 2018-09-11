f70482dbc6a66cf300e476446e3a2f00b9c701a4
Daniil <gatorr15@ya.ru>
2018-09-11 10:48:02 +0300

    Hide credentials in config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$tg_bot_token = '503700120:AAGxJuN9CMFqNjQ2lOsLvtb79T-Llz3H130';
+$db_host = 'eu-cdbr-west-02.cleardb.net';
+$db_username = 'b70a1c22756565';
+$db_pass = '6c429cd3';
+$db_schema = 'heroku_18de73b74f8039e';
+
+?>
