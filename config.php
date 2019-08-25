4fb4fb486e9bd47d870adcd33eb304a9f5283776
MrSnowball-dev <igatorr19@gmail.com>
2019-08-25 04:45:34 +0300

    Update bot token

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$tg_bot_token = '503700120:AAGxJuN9CMFqNjQ2lOsLvtb79T-Llz3H130';
+$tg_bot_token = '503700120:AAHfh-GIFkJDgmRX2RljtFc8pgvnAHgyw5o';
 $db_host = 'eu-cdbr-west-02.cleardb.net';
 $db_username = 'b70a1c22756565';
 $db_pass = '6c429cd3';
